4c04591104573c56526e4211d5ff73dec8f0776a
illeanam <imim26@outlook.com>
2026-09-01 00:31:08 +0000

    Added Last name

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main() {
   int years = 0;
   cout << "First name: ";
   cin >> name;
+  cout << "Last name: ";
+  cin >> name;
   cout << "Years coding goal: ";
   cin >> years;
   cout << name << " — 1/3/5/7 year story goes here (" << years << ")\n";
